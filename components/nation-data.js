d5e7309592623845da58c917b58dd7c551225bd2
geofrontier <mc.geofrontier@gmail.com>
2025-12-12 18:38:55 -0500

    Update nation-data.js

--- components/nation-data.js
+++ components/nation-data.js
@@ -1,1 +1,23 @@
+// components/nation-data.js
+const nationsOfTheMonth = [
+  {
+    month: "December 2025",
+    name: "Republic of Aurelion",
+    image: "images/aurelion.jpg",
+    description: "A rapidly expanding maritime republic known for strong trade networks, disciplined armies, and diplomatic dominance.",
+    population: 42,
+    towns: 6,
+    ideology: "Constitutional Republic"
+  },
+
+  {
+    month: "November 2025",
+    name: "Kingdom of Valemont",
+    image: "images/valemont.jpg",
+    description: "A mountainous monarchy with elite infantry and a fortress-based defensive strategy.",
+    population: 31,
+    towns: 4,
+    ideology: "Absolute Monarchy"
+  }
+];
 
