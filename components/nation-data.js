606bd5fc98e8eb23472a80fabf1bfb37db840c21
geofrontier <mc.geofrontier@gmail.com>
2025-12-12 19:04:41 -0500

    Update nation-data.js

--- components/nation-data.js
+++ components/nation-data.js
@@ -4,7 +4,7 @@ const nationsOfTheMonth = [
     month: "September 2025",
     name: "Salzburg",
     image: "images/salzburg.png",
-    leader: "ShilhohTony, BrusselsGriffon"
+    leader: "ShilhohTony, BrusselsGriffon",
     description: "Named most well-construced town of Season 1; 1 Salzburgian was reportedly equal to 3 random mercenaries.",
     population: 20,
     ideology: "Constitutional Monarchy"
@@ -15,7 +15,7 @@ const nationsOfTheMonth = [
     name: "N/A",
     image: "images/valemont.jpg",
     description: "N/A",
-    leader: "N/A"
+    leader: "N/A",
     population: 31,
     towns: 4,
     ideology: "Absolute Monarchy"
