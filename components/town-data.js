8c685eef3def665f6770d6d40d01d053f8703b53
geofrontier <mc.geofrontier@gmail.com>
2025-12-12 23:39:40 -0500

    Update town-data.js

--- components/town-data.js
+++ components/town-data.js
@@ -5,19 +5,19 @@ const townsOfTheMonth = [
     name: "Salzburg",
     image: "images/salzburg.png",
     leader: "ShilhohTony, BrusselsGriffon",
-    description: "Named most well-construced town of Season 1; 1 Salzburgian was reportedly equal to 3 random mercenaries.",
+    description: "Named most well-construced town of Season 1",
     population: 20,
     ideology: "Constitutional Monarchy"
   },
 
   {
-    month: "N/A",
-    name: "N/A",
+    month: "August 2025",
+    name: "Houston",
     image: "images/valemont.jpg",
-    description: "N/A",
+    description: "Wealthiest",
     leader: "N/A",
-    population: 31,
-    ideology: "Absolute Monarchy"
+    population: 5,
+    ideology: "Dictatorship"
   }
 ];
 
